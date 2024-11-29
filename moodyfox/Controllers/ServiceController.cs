505268636887f4b2d875a751fd0f6eaffb606e3c
Akhilesh Vishwakarma <akhilesh.vis17@gmail.com>
2024-11-29 17:40:56 +0530

    main
    main commit

---
+++ moodyfox/Controllers/ServiceController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace moodyfox.Controllers
+{
+    public class ServiceController : Controller
+    {
+        // GET: Service
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult _3DAnimation()
+        {
+            return View();
+        }
+        public ActionResult _3DProductVideos()
+        {
+            return View();
+        }
+    }
+}
