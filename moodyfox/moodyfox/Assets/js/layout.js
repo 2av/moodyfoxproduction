9e852bba28ebb023c1904ef17e0bbd37aa7abacf
Akhilesh Vishwakarma <akhilesh.vis17@gmail.com>
2024-12-12 19:00:47 +0530

    responsive design changes

---
+++ moodyfox/moodyfox/Assets/js/layout.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+
+    
+   
+    applyJQuery() 
+    function applyJQuery() {
+        if ($(window).width() <= 991) {
+            $(".navbar-collapse").removeClass("wow animate__ animate__fadeInUp animate__animated animated")
+        } 
+    }
+    var $navbar = $(".navbar-collapse").animate({ left: "-100%" }, 300)
+    $(".mob-menu").click(function (e) {
+        e.preventDefault();
+        ;
+
+        if ($navbar.is(":visible")) {
+            $navbar.animate({ left: "-100%" }, 300, function () {
+                $navbar.hide();
+            });
+        } else {
+            $navbar.show().animate({ left: "0" }, 300);
+        }
+    });
+
+});
